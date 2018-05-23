7a43933bafd3648c4f5017df62cbae8dea3b044b
Guilherme <gui.andra142@gmail.com>
2018-05-23 17:31:14 -0300

    Factory e Seeds Categorias e Tags

---
+++ database/seeds/CategoriasTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Categorias::class, 28)->create();
+    }
+}
